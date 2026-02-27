042bdf98b3d223b01b53d3b37969154254b6b59a
Gurehmat <gurehmat9@gmail.com>
2026-02-27 05:30:15 -0500

    fixed navbar name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,7 +53,7 @@ export default function Navbar() {
                                 <ChefHat className="w-6 h-6 text-white" />
                             </div>
                             <span className="text-xl font-bold bg-gradient-to-r from-orange-600 to-red-600 bg-clip-text text-transparent">
-                                PantryChef
+                                PantryCheff
                             </span>
                         </Link>
                     </div>
